e53b489f92a54c926ea4094aae878fc966205c9c
Denis Falqueto <denisfalqueto@gmail.com>
2014-10-02 14:40:21 -0300

    Remover um producer para EntityManager
    Existe um produtor para isto na framework common.

--- plataoear-ejb/src/main/java/br/jus/trt/app/platao/util/Resources.java
+++ plataoear-ejb/src/main/java/br/jus/trt/app/platao/util/Resources.java
@@ -4,8 +4,6 @@ import java.util.logging.Logger;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 /**
  * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
@@ -20,11 +18,6 @@ import javax.persistence.PersistenceContext;
  * </pre>
  */
 public class Resources {
-   // use @SuppressWarnings to tell IDE to ignore warnings about field not being referenced directly
-   @Produces
-   @PersistenceContext
-   private EntityManager em;
-   
    @Produces
    public Logger produceLog(InjectionPoint injectionPoint) {
       return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
