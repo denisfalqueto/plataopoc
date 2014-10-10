9afe5def710275b5f5e64e7f2937e69d98c558f2
Denis Falqueto <denisfalqueto@gmail.com>
2014-10-10 19:47:52 +0000

    Informar o CrudRepository para o ServidorCrudFacade

--- plataoear-ejb/src/main/java/br/jus/trt/app/platao/business/facade/ServidorCrudFacade.java
+++ plataoear-ejb/src/main/java/br/jus/trt/app/platao/business/facade/ServidorCrudFacade.java
@@ -1,7 +1,8 @@
 package br.jus.trt.app.platao.business.facade;
 
 import br.jus.trt.app.platao.business.domain.Servidor;
+import br.jus.trt.app.platao.integration.persistence.ServidorRepository;
 import br.jus.trt.lib.common_core.business.facade.CrudFacadeBase;
 
-public class ServidorCrudFacade extends CrudFacadeBase<Servidor, Long> {
+public class ServidorCrudFacade extends CrudFacadeBase<ServidorRepository, Servidor, Long> {
 }
