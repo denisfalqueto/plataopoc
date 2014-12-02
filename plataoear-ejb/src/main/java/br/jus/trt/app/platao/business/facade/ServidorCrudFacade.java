ae3b64ac18e0ab1e3a96d0a9b3e5a78bb12044a4
Denis Falqueto <denisfalqueto@gmail.com>
2014-12-02 13:44:35 +0000

    Colocar @Stateless na classe concreta
    Isto garante que o objeto vai estar associado a um contexto transacional
    
    A anotação @Transactional do JEE7 não funcionou como esperado.

--- plataoear-ejb/src/main/java/br/jus/trt/app/platao/business/facade/ServidorCrudFacade.java
+++ plataoear-ejb/src/main/java/br/jus/trt/app/platao/business/facade/ServidorCrudFacade.java
@@ -3,7 +3,12 @@ package br.jus.trt.app.platao.business.facade;
 import br.jus.trt.app.platao.business.domain.Servidor;
 import br.jus.trt.app.platao.integration.persistence.ServidorRepository;
 import br.jus.trt.lib.common_core.business.facade.CrudFacadeBase;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 
+@Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
 public class ServidorCrudFacade extends CrudFacadeBase<ServidorRepository, Servidor, Long> {
 	 
 }
